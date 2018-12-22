7a3a0a00a0f4401d526e5fcaee073df54647d6d3
Ankiii001 <ankithbti177@gmail.com>
2018-12-22 23:49:30 +0530

    Request mapping with params

--- PSBankApp/WebContent/WEB-INF/Views/testRequestmapping.jsp
+++ PSBankApp/WebContent/WEB-INF/Views/testRequestmapping.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h2>Test Request mapping</h2>
+	<h2>Test Request mapping</h2>
+	<h3>Welcome ${userName}</h3>
 </body>
 </html>
--- PSBankApp/src/com/nagarro/springmvc/psbankapp/TestController.java
+++ PSBankApp/src/com/nagarro/springmvc/psbankapp/TestController.java
@@ -1,7 +1,9 @@
 package com.nagarro.springmvc.psbankapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/test")
@@ -11,5 +13,12 @@ public class TestController {
 	public String index() {
 		return "testRequestmapping";
 		}
+
+	@RequestMapping(value= "/name")
+	public String methodWithParams(@RequestParam String userName, Model model) {
+	model.addAttribute("userName",userName);	
+	System.out.println("Page requested by " + userName);
+	return "testRequestmapping";
+	}
 
 }
