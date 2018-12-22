ad921c30cb2c1769cc7a86658f89a19ba4fd27df
Ankiii001 <ankithbti177@gmail.com>
2018-12-23 00:06:27 +0530

    @RequestMapping with Dynamic URLs

--- PSBankApp/src/com/nagarro/springmvc/psbankapp/TestController.java
+++ PSBankApp/src/com/nagarro/springmvc/psbankapp/TestController.java
@@ -2,24 +2,29 @@ package com.nagarro.springmvc.psbankapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
-	
-	@RequestMapping(value= {"","/index","/index/*"})
+
+	@RequestMapping(value = { "", "/index", "/index/*" })
 	public String index() {
 		return "testRequestmapping";
-		}
-
-	@RequestMapping(value= "/name")
-	public String methodWithParams(@RequestParam(value="userName" , required=false, defaultValue="Guest")
-	String userName, Model model) {
-	model.addAttribute("userName",userName);	
-	System.out.println("Page requested by " + userName);
-	return "testRequestmapping";
 	}
 
+	@RequestMapping(value = "/name")
+	public String methodWithParams(
+			@RequestParam(value = "userName", required = false, defaultValue = "Guest") String userName, Model model) {
+		model.addAttribute("userName", userName);
+		System.out.println("Page requested by " + userName);
+		return "testRequestmapping";
+	}
+	
+	@RequestMapping(value = "/dynamic/{userName}")
+	public String dynamicURL(@PathVariable("userName") String userName) {
+		return "testRequestmapping";
+	}
 }
